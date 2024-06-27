84fe2679537aacadc1b95cdae4effed62a4e8fba
joshuanelsondev <joshuanelsondev@gmail.com>
2024-06-27 00:03:58 -0400

    feat: edit header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,21 @@
+import Icon from '@mdi/react';
+import { mdiBookOpenBlankVariantOutline, mdiBellOutline } from '@mdi/js';
+
 import './Header.scss'
 
 export default function Header () {
-  return <div>Header</div>
+  return (
+    <div className="header">
+      <h2 className="header-logo">Online Library</h2>
+      <form className="search">
+        <label htmlFor="search"></label>
+        <input type="search" id="search" className="search__input" placeholder="Search books, authors, ISBNs, publishers..." />
+        <button className="search__button">Search</button>
+      </form>
+      <p>Browse</p>
+      <p>MyBooks</p>
+      <Icon path={mdiBookOpenBlankVariantOutline} size={1} />
+      <Icon path={mdiBellOutline} size={1} />
+    </div>
+  )
 }
