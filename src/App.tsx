22025f1a58ddc3547429740cfe12538f9081d97d
joshuanelsondev <joshuanelsondev@gmail.com>
2024-06-27 20:00:09 -0400

    feat: style header

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ function App() {
 
   return (
     <main className='main-container'>
-      <Browse />
       <Header />
+      <Browse />
       <Sidebar />
     </main>
   )
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,21 +1,20 @@
-import Icon from '@mdi/react';
-import { mdiBookOpenBlankVariantOutline, mdiBellOutline } from '@mdi/js';
+import { LuBell, LuBookOpen } from "react-icons/lu";
 import profileIcon from '../../assets/profile-icon.jpeg'
 import './Header.scss'
 
 export default function Header () {
   return (
     <div className="header">
-      <h2 className="header_logo">Online Library</h2>
+      <h2 className="header__logo">Online Library</h2>
       <form className="header__search-form">
         <label htmlFor="search"></label>
         <input type="search" id="search" className="header__search-input" placeholder="Search books, authors, ISBNs, publishers..." />
         <button className="header__search-btn">Search</button>
       </form>
       <p className='header__browse'>Browse</p>
-      <p className='header__mybooks'>MyBooks</p>
-      <Icon className='header__icon' path={mdiBookOpenBlankVariantOutline} size={1} />
-      <Icon className='header__icon' path={mdiBellOutline} size={1} />
+      <p className='header__mybooks'>My Books</p>
+      <LuBookOpen className='header__icon' size={1.5} />
+      <LuBell className='header__icon' size={1.5} />
       <img className='header__profile-icon' src={profileIcon} alt="profile icon" />
     </div>
   )
