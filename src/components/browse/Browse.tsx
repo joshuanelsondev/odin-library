c3326ad793a17bbde4c8ed3fb09f4f20b693fa6f
joshuanelsondev <joshuanelsondev@gmail.com>
2024-06-29 14:32:23 -0400

    feat: add Year and Status dropdowns

--- src/components/browse/Browse.tsx
+++ src/components/browse/Browse.tsx
@@ -5,7 +5,8 @@ export default function Browse () {
     <div className='browse'>
       <h2 className='browse__header'>Browse The Library</h2>
       <div className='browse__filter' >
-       <select id="genre" name="genre">
+      {/* Genre dropdown - loop through the current available genres */}
+       <select className='browse__select' id="genre" name="genre">
          <option>Genre</option>
          <optgroup label='Fiction'>
            <option value="romance">Romance</option>
@@ -25,6 +26,16 @@ export default function Browse () {
          </optgroup>
          <option value="poetry">Poetry</option>
          <option value="short-story">Short Story</option>
+       </select>
+      {/* Year dropdown - loop through the years of the current available books */}
+       <select className='browse__select' id="year" name="year">
+         <option>Year</option>
+       </select>
+      {/* Status dropdown */}
+       <select className='browse__select' id="status" name="status">
+         <option>Status</option>
+         <option value='read'>Read</option>
+         <option value='unread'>Unread</option>
        </select>
       </div>
     </div>
