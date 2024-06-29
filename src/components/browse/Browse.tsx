25772914d48726ae1d644f95a3bab4a39397ee19
joshuanelsondev <joshuanelsondev@gmail.com>
2024-06-29 01:26:32 -0400

    refactor: update class names

--- src/components/browse/Browse.tsx
+++ src/components/browse/Browse.tsx
@@ -1,5 +1,12 @@
 import './Browse.scss'
 
 export default function Browse () {
-  return <div className='browse'>Browse</div>
+  return (
+    <div className='browse'>
+      <h2 className='browse__header'>Browse The Library</h2>
+      <div className='browse__filter' >
+
+      </div>
+    </div>
+  )
 }
