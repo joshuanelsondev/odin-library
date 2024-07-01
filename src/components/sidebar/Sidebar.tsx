1c3ab1e817312dd2fb4fdb842fad12cf7f829c25
joshuanelsondev <joshuanelsondev@gmail.com>
2024-06-30 21:41:56 -0400

    feat: add layout features to sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,5 +1,17 @@
 import './Sidebar.scss'
 
 export default function Sidebar () {
-  return <div className='sidebar'>Sidebar</div>
+  return(
+    <div className='sidebar'>
+      <h3>About This Book</h3>
+      <img src="" alt="" />
+      <p>Title:</p>
+      <p>Author(s):</p>
+      <p>ISBN:</p>
+      <p>Publisher:</p>
+      <p>Published Date:</p>
+      <p>Summary</p>
+      <button>Check Out</button>
+    </div>
+  )
 }
