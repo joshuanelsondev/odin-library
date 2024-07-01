316bf96582636192ee0f09176f9d8834d0967edc
joshuanelsondev <joshuanelsondev@gmail.com>
2024-06-30 23:17:33 -0400

    feat: edit layout and style of sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -5,12 +5,12 @@ export default function Sidebar () {
     <div className='sidebar'>
       <h3 className='sidebar__header'>About This Book</h3>
       <img className='sidebar__book' src="" alt="" />
-      <p className='sidebar__info'>Title:</p>
-      <p className='sidebar__info'>Author(s):</p>
-      <p className='sidebar__info'>ISBN:</p>
-      <p className='sidebar__info'>Publisher:</p>
-      <p className='sidebar__info'>Published Date:</p>
-      <p className='sidebar__info'>Summary</p>
+      <p className='sidebar__info'>Title:<span className='sidebar__text'>This is temporary text</span></p>
+      <p className='sidebar__info'>Author(s):<span className='sidebar__text'>This is temporary text</span></p>
+      <p className='sidebar__info'>ISBN:<span className='sidebar__text'>This is temporary text</span></p>
+      <p className='sidebar__info'>Publisher:<span className='sidebar__text'>This is temporary text</span></p>
+      <p className='sidebar__info'>Published Date:<span className='sidebar__text'>This is temporary text</span></p>
+      <p className='sidebar__info'>Summary<span className='sidebar__text'>This is temporary text</span></p>
       <button className='sidebar__button'>Check Out</button>
     </div>
   )
