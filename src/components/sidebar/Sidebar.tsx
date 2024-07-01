76cbd360e54e60bb390fd9ea71acad24545b376e
joshuanelsondev <joshuanelsondev@gmail.com>
2024-06-30 21:48:49 -0400

    style: add classNames to sidebar jsx tags

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -3,15 +3,15 @@ import './Sidebar.scss'
 export default function Sidebar () {
   return(
     <div className='sidebar'>
-      <h3>About This Book</h3>
-      <img src="" alt="" />
-      <p>Title:</p>
-      <p>Author(s):</p>
-      <p>ISBN:</p>
-      <p>Publisher:</p>
-      <p>Published Date:</p>
-      <p>Summary</p>
-      <button>Check Out</button>
+      <h3 className='sidebar__header'>About This Book</h3>
+      <img className='sidebar__book' src="" alt="" />
+      <p className='sidebar__info'>Title:</p>
+      <p className='sidebar__info'>Author(s):</p>
+      <p className='sidebar__info'>ISBN:</p>
+      <p className='sidebar__info'>Publisher:</p>
+      <p className='sidebar__info'>Published Date:</p>
+      <p className='sidebar__info'>Summary</p>
+      <button className='sidebar__button'>Check Out</button>
     </div>
   )
 }
