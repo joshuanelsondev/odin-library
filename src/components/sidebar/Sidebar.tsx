d08a98fdeebe0e2f15a4af84ca1793cc0b3a9dc1
joshuanelsondev <joshuanelsondev@gmail.com>
2024-07-01 23:47:04 -0400

    feat: add favorite button

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,10 +1,12 @@
+import { FaHeart } from "react-icons/fa";
 import './Sidebar.scss'
 
 export default function Sidebar () {
   return(
     <div className='sidebar'>
       <h3 className='sidebar__header'>About This Book</h3>
-      <img className='sidebar__book' src="" alt="" />
+      <img className='sidebar__book' src="" alt=""/>
+      <FaHeart className='sidebar__heart'/>
       <p className='sidebar__info'>Title:<span className='sidebar__text'>This is temporary text</span></p>
       <p className='sidebar__info'>Author(s):<span className='sidebar__text'>This is temporary text</span></p>
       <p className='sidebar__info'>ISBN:<span className='sidebar__text'>This is temporary text</span></p>
