f65915dbd50a09155c54b4cf48e3cda5202dac88
joshuanelsondev <joshuanelsondev@gmail.com>
2024-07-05 00:06:11 -0400

    feat: list out books to be added to library

--- src/components/browse/library.ts
+++ src/components/browse/library.ts
@@ -1,3 +1,8 @@
 /*
-ExtraOrdinary: V.E. Schwab, Enid Balam  
+- ExtraOrdinary:: V.E. Schwab, Enid Balam  
+- Another Kind:: Trevor Bream
+- The High 5 Habit: Take...:: Mel Robbins
+- The Perfect Day to...:: Rick Ross
+- Sapiens: A Graphic...:: David Vandermeulen, ...
+- Peak Mind: Find Your...:: Amishi P. Jha
 */
