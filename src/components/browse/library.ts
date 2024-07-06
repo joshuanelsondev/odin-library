1d4ea2585e7ea2313c60f609c7cebb3f72c80084
joshuanelsondev <joshuanelsondev@gmail.com>
2024-07-06 00:33:24 -0400

    feat: add stock books to library.ts

--- src/components/browse/library.ts
+++ src/components/browse/library.ts
@@ -6,16 +6,16 @@
 - Sapiens: A Graphic...:: David Vandermeulen, ...
 - Peak Mind: Find Your...:: Amishi P. Jha
 */
-interface Library {
+interface BookDetails {
  title: string
  author: string
  isbn: string
- publisher: strning
+ publisher: string
  publisher_date: string
  summary: string
 }
 
-const myLibrary: Library = [
+const myLibrary: BookDetails = [
   {
     title: 'ExtraOrdinary, (Villains #1.5)',
     author: 'V.E. Schwab, Enid Balam',
@@ -25,43 +25,43 @@ const myLibrary: Library = [
     summary: 'Sit similique quas iste veritatis voluptates Accusantium sequi non voluptates.'
   },
   {
-    title: 'ExtraOrdinary, (Villains #1.5)',
-    author: 'V.E. Schwab, Enid Balam',
-    isbn: '1785865889',
-    publisher: 'Titan Comics',
-    publisher_date: '10-2-2021',
-    summary: 'Sit similique quas iste veritatis voluptates Accusantium sequi non voluptates.'
+    title: 'Another Kind',
+    author: 'Trevor Bream',
+    isbn: '006304353X',
+    publisher: 'HarperAlley',
+    publisher_date: '10-26-2021',
+    summary: 'Amet facilis similique culpa non commodi nam? Error facere ipsa'
   },
   {
-    title: 'ExtraOrdinary, (Villains #1.5)',
-    author: 'V.E. Schwab, Enid Balam',
-    isbn: '1785865889',
-    publisher: 'Titan Comics',
-    publisher_date: '10-2-2021',
-    summary: 'Sit similique quas iste veritatis voluptates Accusantium sequi non voluptates.'
+    title: 'The High 5 habit: Take Control of Your Life with One Simple Habit',
+    author: 'Mel Robbins',
+    isbn: '1401962122',
+    publisher: 'Hay House Inc.',
+    publisher_date: '9-28-2021',
+    summary: 'Adipisicing temporibus illum sit sapiente est.'
   },
   {
-    title: 'ExtraOrdinary, (Villains #1.5)',
-    author: 'V.E. Schwab, Enid Balam',
-    isbn: '1785865889',
-    publisher: 'Titan Comics',
-    publisher_date: '10-2-2021',
-    summary: 'Sit similique quas iste veritatis voluptates Accusantium sequi non voluptates.'
+    title: 'The Perfect Day to Boss Up',
+    author: 'Rick Ross',
+    isbn: '0369705793',
+    publisher: 'Hanover Square Press',
+    publisher_date: '9-7-2021',
+    summary: 'Lorem numquam cumque eos repudiandae molestias ad similique.'
   },
   {
-    title: 'ExtraOrdinary, (Villains #1.5)',
-    author: 'V.E. Schwab, Enid Balam',
-    isbn: '1785865889',
-    publisher: 'Titan Comics',
-    publisher_date: '10-2-2021',
-    summary: 'Sit similique quas iste veritatis voluptates Accusantium sequi non voluptates.'
+    title: 'Sapiens: A Graphic History, Volume 2 - The Pillars of Civilization',
+    author: 'David Vandermeulen, Daniel Casanave, Yuval Noah Harari',
+    isbn: '0369705793',
+    publisher: 'Harper Perennial',
+    publisher_date: '10,26,2021',
+    summary: 'Lorem repellendus accusantium enim ex atque Alias praesentium quibusdam neque fugiat'
   },
   {
-    title: 'ExtraOrdinary, (Villains #1.5)',
-    author: 'V.E. Schwab, Enid Balam',
-    isbn: '1785865889',
-    publisher: 'Titan Comics',
-    publisher_date: '10-2-2021',
-    summary: 'Sit similique quas iste veritatis voluptates Accusantium sequi non voluptates.'
+    title: 'Peak Mind: Find Your Focus, Own Your Attention, Invest 12 Minutes a Day',
+    author: 'Amishi P. Jha',
+    isbn: '0062992163',
+    publisher: 'HarperOne',
+    publisher_date: '10-19-2021',
+    summary: 'Elit velit provident eaque odit iste.'
   }
 ]
