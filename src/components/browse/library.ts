71dec7e87e5ed0d08612e2d714de8aa61d135b92
joshuanelsondev <joshuanelsondev@gmail.com>
2024-07-05 23:47:17 -0400

    create layout for book object structure

--- src/components/browse/library.ts
+++ src/components/browse/library.ts
@@ -6,3 +6,62 @@
 - Sapiens: A Graphic...:: David Vandermeulen, ...
 - Peak Mind: Find Your...:: Amishi P. Jha
 */
+interface Library {
+ title: string
+ author: string
+ isbn: string
+ publisher: strning
+ publisher_date: string
+ summary: string
+}
+
+const myLibrary: Library = [
+  {
+    title: 'ExtraOrdinary, (Villains #1.5)',
+    author: 'V.E. Schwab, Enid Balam',
+    isbn: '1785865889',
+    publisher: 'Titan Comics',
+    publisher_date: '10-2-2021',
+    summary: 'Sit similique quas iste veritatis voluptates Accusantium sequi non voluptates.'
+  },
+  {
+    title: 'ExtraOrdinary, (Villains #1.5)',
+    author: 'V.E. Schwab, Enid Balam',
+    isbn: '1785865889',
+    publisher: 'Titan Comics',
+    publisher_date: '10-2-2021',
+    summary: 'Sit similique quas iste veritatis voluptates Accusantium sequi non voluptates.'
+  },
+  {
+    title: 'ExtraOrdinary, (Villains #1.5)',
+    author: 'V.E. Schwab, Enid Balam',
+    isbn: '1785865889',
+    publisher: 'Titan Comics',
+    publisher_date: '10-2-2021',
+    summary: 'Sit similique quas iste veritatis voluptates Accusantium sequi non voluptates.'
+  },
+  {
+    title: 'ExtraOrdinary, (Villains #1.5)',
+    author: 'V.E. Schwab, Enid Balam',
+    isbn: '1785865889',
+    publisher: 'Titan Comics',
+    publisher_date: '10-2-2021',
+    summary: 'Sit similique quas iste veritatis voluptates Accusantium sequi non voluptates.'
+  },
+  {
+    title: 'ExtraOrdinary, (Villains #1.5)',
+    author: 'V.E. Schwab, Enid Balam',
+    isbn: '1785865889',
+    publisher: 'Titan Comics',
+    publisher_date: '10-2-2021',
+    summary: 'Sit similique quas iste veritatis voluptates Accusantium sequi non voluptates.'
+  },
+  {
+    title: 'ExtraOrdinary, (Villains #1.5)',
+    author: 'V.E. Schwab, Enid Balam',
+    isbn: '1785865889',
+    publisher: 'Titan Comics',
+    publisher_date: '10-2-2021',
+    summary: 'Sit similique quas iste veritatis voluptates Accusantium sequi non voluptates.'
+  }
+]
