b8e3c76400e72eb2d0f04baffc80f905700e1f64
joshuanelsondev <joshuanelsondev@gmail.com>
2024-07-15 23:45:31 -0400

    feat: add class names to AddBookForm and position the form component

--- src/components/addBook/AddBookForm.tsx
+++ src/components/addBook/AddBookForm.tsx
@@ -1,4 +1,5 @@
 import { LuX } from "react-icons/lu";
+import './AddBookForm.scss'
 
 interface FormProps {
   setFormVisibility: (visibility: boolean) => void
@@ -13,13 +14,15 @@ export default function AddBookForm ({ setFormVisibility }: FormProps) {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <h2>Add Book</h2>
-        <LuX onClick={() => setFormVisibility(false)} />
-      </div>
+    <div className='addbook-container'>
+      <form className='addbook-form' onSubmit={handleSubmit}>
+        <div>
+          <h2>Add Book</h2>
+          <LuX onClick={() => setFormVisibility(false)} />
+        </div>
 
-      <button type='submit'>Submit</button>
-    </form>
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
   )
 }
