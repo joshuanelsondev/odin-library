e96d1d1cc58b998254b5884a7bf99f86cf577a7d
joshuanelsondev <joshuanelsondev@gmail.com>
2024-07-16 16:25:13 -0400

    feat: add form properties to AddBookForm

--- src/components/addBook/AddBookForm.tsx
+++ src/components/addBook/AddBookForm.tsx
@@ -20,6 +20,34 @@ export default function AddBookForm ({ setFormVisibility }: FormProps) {
           <h2 className='formModal__header'>Add Book</h2>
           <LuX className='formModal__close' onClick={() => setFormVisibility(false)} size={20} />
         </div>
+        <div>
+          <label htmlFor="title">Title: </label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="author">Author: </label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="isbn">ISBN: </label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="publisher">Publisher: </label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="published-date">Published Date: </label>
+          <input type="month" />
+        </div>
+        <div>
+          <label htmlFor="summary">Summary: </label>
+          <textarea id="summary" name="summary" cols={30} rows={10}></textarea>
+        </div>
+        <div>
+          <label htmlFor="book-cover-url">Book Cover Url: </label>
+          <input type="url" />
+        </div>
         <div className='formModal__body'></div>
         <div className='formModal__footer'>
           <button className='formModal__cancel' type='submit'>Cancel</button>
