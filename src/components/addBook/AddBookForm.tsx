5fac9e6a8850a5513fe000587673c2e32b5e078f
joshuanelsondev <joshuanelsondev@gmail.com>
2024-07-18 22:51:00 -0400

    refactor: restructure form body in AddBookForm and style body of form

--- src/components/addBook/AddBookForm.tsx
+++ src/components/addBook/AddBookForm.tsx
@@ -20,35 +20,36 @@ export default function AddBookForm ({ setFormVisibility }: FormProps) {
           <h2 className='formModal__header'>Add Book</h2>
           <LuX className='formModal__close' onClick={() => setFormVisibility(false)} size={20} />
         </div>
-        <div className='formModal__text-input'>
-          <label htmlFor="title">Title: </label>
-          <input type="text" />
+        <div className='formModal__body'>
+          <div className='formModal__input-container'>
+            <label htmlFor="title">Title: </label>
+            <input type="text" />
+          </div>
+          <div className='formModal__input-container'>
+            <label htmlFor="author">Author: </label>
+            <input type="text" />
+          </div>
+          <div className='formModal__input-container'>
+            <label htmlFor="isbn">ISBN: </label>
+            <input type="text" />
+          </div>
+          <div className='formModal__input-container'>
+            <label htmlFor="publisher">Publisher: </label>
+            <input type="text" />
+          </div>
+          <div className='formModal__input-container'>
+            <label htmlFor="published-date">Published Date: </label>
+            <input type="date" />
+          </div>
+          <div className='formModal__input-container'>
+            <label htmlFor="book-cover-url">Book Cover Url: </label>
+            <input type="url" />
+          </div>
+          <div className='formModal__summary'>
+            <label htmlFor="summary">Summary: </label>
+            <textarea id="summary" name="summary" cols={30} rows={10}></textarea>
+          </div>
         </div>
-        <div className='formModal__text-input'>
-          <label htmlFor="author">Author: </label>
-          <input type="text" />
-        </div>
-        <div className='formModal__text-input'>
-          <label htmlFor="isbn">ISBN: </label>
-          <input type="text" />
-        </div>
-        <div className='formModal__text-input'>
-          <label htmlFor="publisher">Publisher: </label>
-          <input type="text" />
-        </div>
-        <div className='formModal__date-input'>
-          <label htmlFor="published-date">Published Date: </label>
-          <input type="month" />
-        </div>
-        <div className='formModal__textarea'>
-          <label htmlFor="summary">Summary: </label>
-          <textarea id="summary" name="summary" cols={30} rows={10}></textarea>
-        </div>
-        <div className='formModal__url'>
-          <label htmlFor="book-cover-url">Book Cover Url: </label>
-          <input type="url" />
-        </div>
-        <div className='formModal__body'></div>
         <div className='formModal__footer'>
           <button className='formModal__cancel' type='submit'>Cancel</button>
           <button className='formModal__submit' type='submit'>Add Book</button>
