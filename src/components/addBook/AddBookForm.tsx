35e55a6ee26a9754ca6370f3e7c00481676b82da
joshuanelsondev <joshuanelsondev@gmail.com>
2024-07-20 21:47:34 -0400

    feat: add handleCheckboxChange function and add attributes to input elements
    refactor: update handleFormChange to handleTextChange

--- src/components/addBook/AddBookForm.tsx
+++ src/components/addBook/AddBookForm.tsx
@@ -20,9 +20,14 @@ export default function AddBookForm ({ setFormVisibility }: FormProps) {
     read: false,
   })
 
-  const handleFormChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target
-    setBook({...book, [name]: value})
+  const handleTextChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { id, value } = event.target
+    setBook({...book, [id]: value})
+  }
+
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, checked } = event.target
+    setBook({...book, [id]: checked})
   }
 
   const handleSubmit = (event: React.FormEvent) => {
@@ -42,39 +47,39 @@ export default function AddBookForm ({ setFormVisibility }: FormProps) {
         <div className='formModal__body'>
           <div className='formModal__input-container'>
             <label htmlFor="title">Title: </label>
-            <input type="text" />
+            <input type="text" id='title' name='title' value={book.title} onChange={handleTextChange}  />
           </div>
           <div className='formModal__input-container'>
             <label htmlFor="author">Author: </label>
-            <input type="text" />
+            <input type="text" id='author' name='author' value={book.author} onChange={handleTextChange}/>
           </div>
           <div className='formModal__input-container'>
             <label htmlFor="isbn">ISBN: </label>
-            <input type="text" />
+            <input type="text" id='isbn' name='isbn' value={book.isbn} onChange={handleTextChange}/>
           </div>
           <div className='formModal__input-container'>
             <label htmlFor="publisher">Publisher: </label>
-            <input type="text" />
+            <input type="text" id='publisher' name='publisher' value={book.publisher} onChange={handleTextChange}/>
           </div>
           <div className='formModal__input-container'>
-            <label htmlFor="published-date">Published Date: </label>
-            <input type="date" />
+            <label htmlFor="published_date">Published Date: </label>
+            <input type="date" id='published_date' name='published_date' value={book.published_date} onChange={handleTextChange}/>
           </div>
           <div className='formModal__input-container'>
-            <label htmlFor="book-cover-url">Book Cover Url: </label>
-            <input type="url" />
+            <label htmlFor="cover_image_url">Book Cover Url: </label>
+            <input type="url" id='cover_image_url' name='cover_image_url' value={book.cover_image_url} onChange={handleTextChange}/>
           </div>
           <div>
             <label htmlFor="favorite">Favorite: </label>
-            <input type="checkbox" id='favorite' name='favorite' />
+            <input type="checkbox" id='favorite' name='favorite' checked={book.favorite} onChange={handleCheckboxChange} />
           </div>
           <div>
             <label htmlFor="read">Read: </label>
-            <input type="checkbox" id='read' name='read' />
+            <input type="checkbox" id='read' name='read' checked={book.read} onChange={handleCheckboxChange}/>
           </div>
           <div className='formModal__summary'>
             <label htmlFor="summary">Summary: </label>
-            <textarea id="summary" name="summary" cols={30} rows={10}></textarea>
+            <textarea id="summary" name="summary" value={book.summary} onChange={handleTextChange} cols={30} rows={10}></textarea>
           </div>
         </div>
         <div className='formModal__footer'>
