41df40ebba3f2615135364e122e5e61141ff9f58
joshuanelsondev <joshuanelsondev@gmail.com>
2024-07-22 23:16:05 -0400

    refactor: update the default books in library.ts

--- src/assets/library.ts
+++ src/assets/library.ts
@@ -1,70 +1,70 @@
-import { Book } from '../../../types/Book'
+import { Book } from '../types/Book'
 
 export const myLibrary: Book[] = [
-  {
-    title: 'ExtraOrdinary, (Villains #1.5)',
-    author: 'V.E. Schwab, Enid Balam',
-    isbn: '1785865889',
-    publisher: 'Titan Comics',
-    published_date: '10-2-2021',
-    summary: 'Sit similique quas iste veritatis voluptates Accusantium sequi non voluptates.',
-    cover_image_url: 'src/assets/bookCovers/ExtraOrdinary.png',
-    is_favorite: false,
-    read: true
-  },
-  {
-    title: 'Another Kind',
-    author: 'Trevor Bream',
-    isbn: '006304353X',
-    publisher: 'HarperAlley',
-    published_date: '10-26-2021',
-    summary: 'Amet facilis similique culpa non commodi nam? Error facere ipsa',
-    cover_image_url: 'src/assets/bookCovers/Another_Kind.png',
-    is_favorite: false,
-    read: false
-  },
-  {
-    title: 'The High 5 habit: Take Control of Your Life with One Simple Habit',
-    author: 'Mel Robbins',
-    isbn: '1401962122',
-    publisher: 'Hay House Inc.',
-    published_date: '09-28-2021',
-    summary: 'Adipisicing temporibus illum sit sapiente est.',
-    cover_image_url: 'src/assets/bookCovers/The_High_5_Habit.png',
-    is_favorite: false,
-    read: true
-  },
-  {
-    title: 'The Perfect Day to Boss Up',
-    author: 'Rick Ross',
-    isbn: '0369705793',
-    publisher: 'Hanover Square Press',
-    published_date: '09-7-2021',
-    summary: 'Lorem numquam cumque eos repudiandae molestias ad similique.',
-    cover_image_url: 'src/assets/bookCovers/The_Perfect_Day_To....png',
-    is_favorite: false,
-    read: true
-  },
-  {
-    title: 'Sapiens: A Graphic History, Volume 2 - The Pillars of Civilization',
-    author: 'David Vandermeulen, Daniel Casanave, Yuval Noah Harari',
-    isbn: '0063212234',
-    publisher: 'Harper Perennial',
-    published_date: '10,26,2021',
-    summary: 'Lorem repellendus accusantium enim ex atque Alias praesentium quibusdam neque fugiat',
-    cover_image_url: 'src/assets/bookCovers/Sapiens_A_Graphic_History.png',
-    is_favorite: false,
-    read: false
-  },
-  {
-    title: 'Peak Mind: Find Your Focus, Own Your Attention, Invest 12 Minutes a Day',
-    author: 'Amishi P. Jha',
-    isbn: '0062992163',
-    publisher: 'HarperOne',
-    published_date: '10-19-2021',
-    summary: 'Elit velit provident eaque odit iste.',
-    cover_image_url: 'src/assets/bookCovers/Peak_Mind_Find_Your_Focus.png',
-    is_favorite: false,
-    read: false
-  }
+  new Book(
+    'ExtraOrdinary, (Villains #1.5)',
+    'V.E. Schwab, Enid Balam',
+    '1785865889',
+    'Titan Comics',
+    '10-2-2021',
+    'Sit similique quas iste veritatis voluptates Accusantium sequi non voluptates.',
+    'src/assets/bookCovers/ExtraOrdinary.png',
+    false,
+    true
+  ),
+  new Book(
+    'Another Kind',
+    'Trevor Bream',
+    '006304353X',
+    'HarperAlley',
+    '10-26-2021',
+    'Amet facilis similique culpa non commodi nam? Error facere ipsa',
+    'src/assets/bookCovers/Another_Kind.png',
+    false,
+    false
+  ),
+  new Book(
+    'The High 5 habit: Take Control of Your Life with One Simple Habit',
+    'Mel Robbins',
+    '1401962122',
+    'Hay House Inc.',
+    '09-28-2021',
+    'Adipisicing temporibus illum sit sapiente est.',
+    'src/assets/bookCovers/The_High_5_Habit.png',
+    false,
+    true
+  ),
+  new Book(
+    'The Perfect Day to Boss Up',
+    'Rick Ross',
+    '0369705793',
+    'Hanover Square Press',
+    '09-7-2021',
+    'Lorem numquam cumque eos repudiandae molestias ad similique.',
+    'src/assets/bookCovers/The_Perfect_Day_To....png',
+    false,
+    true
+  ),
+  new Book(
+    'Sapiens: A Graphic History, Volume 2 - The Pillars of Civilization',
+    'David Vandermeulen, Daniel Casanave, Yuval Noah Harari',
+    '0063212234',
+    'Harper Perennial',
+    '10-26-2021',
+    'Lorem repellendus accusantium enim ex atque Alias praesentium quibusdam neque fugiat',
+    'src/assets/bookCovers/Sapiens_A_Graphic_History.png',
+    false,
+    false
+  ),
+  new Book(
+    'Peak Mind: Find Your Focus, Own Your Attention, Invest 12 Minutes a Day',
+    'Amishi P. Jha',
+    '0062992163',
+    'HarperOne',
+    '10-19-2021',
+    'Elit velit provident eaque odit iste.',
+    'src/assets/bookCovers/Peak_Mind_Find_Your_Focus.png',
+    false,
+    false
+  )
 ]
