c0e0e1ddbca75184ad068668fe807c9609dbee2f
joshuanelsondev <joshuanelsondev@gmail.com>
2024-07-22 21:32:07 -0400

    feat: create a Book class and implement BookDetails

--- src/types/Book.ts
+++ src/types/Book.ts
@@ -1,4 +1,4 @@
-export interface Book {
+export interface BookDetails {
  title: string
  author: string
  isbn: string
@@ -8,4 +8,18 @@ export interface Book {
  cover_image_url: string
  favorite: boolean
  read: boolean
+}
+
+export class Book implements BookDetails {
+  constructor(
+    public title: string,
+    public author: string,
+    public isbn: string,
+    public publisher: string,
+    public published_date: string,
+    public summary: string,
+    public cover_image_url: string,
+    public favorite: boolean = false,
+    public read: boolean = false
+  ) {}
 }
