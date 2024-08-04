e32d880fd5803d752931325c9c44ad78d9cce87b
joshuanelsondev <joshuanelsondev@gmail.com>
2024-08-04 11:09:20 -0400

    refactor: use preventDefault on search form and style search input

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,21 +1,34 @@
-import { LuBell, LuBookOpen } from "react-icons/lu";
+import { LuBell, LuBookOpen } from 'react-icons/lu'
 import profileIcon from '../../assets/profile-icon.jpeg'
 import './Header.scss'
 
-export default function Header () {
+export default function Header() {
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+  }
+
   return (
     <div className="header">
       <h2 className="header__logo">Online Library</h2>
-      <form className="header__search-form">
+      <form className="header__search-form" onSubmit={handleSubmit}>
         <label htmlFor="search"></label>
-        <input type="search" id="search" className="header__search-input" placeholder="Search books, authors, ISBNs, publishers..." />
+        <input
+          type="search"
+          id="search"
+          className="header__search-input"
+          placeholder="Search books, authors, ISBNs, publishers..."
+        />
         <button className="header__search-btn">Search</button>
       </form>
-      <p className='header__browse'>Browse</p>
-      <p className='header__mybooks'>My Books</p>
-      <LuBookOpen className='header__icon' size={1} />
-      <LuBell className='header__icon' size={1} />
-      <img className='header__profile-icon' src={profileIcon} alt="profile icon" />
+      <p className="header__browse">Browse</p>
+      <p className="header__mybooks">My Books</p>
+      <LuBookOpen className="header__icon" size={1} />
+      <LuBell className="header__icon" size={1} />
+      <img
+        className="header__profile-icon"
+        src={profileIcon}
+        alt="profile icon"
+      />
     </div>
   )
 }
